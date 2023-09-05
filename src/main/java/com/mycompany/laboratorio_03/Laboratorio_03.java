95f16b409b6683bc115f8003376fbb9a9dbfe28b
LizzAsenjo <142689925+LizzAsenjo@users.noreply.github.com>
2023-09-04 19:49:58 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/laboratorio_03/Laboratorio_03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio_03;
+
+/**
+ *
+ * @author Liz
+ */
+public class Laboratorio_03 {
+
+    public static void main(String[] args) {
+        
+        FListaPersona pantalla = new FListaPersona();
+        pantalla.setLocationRelativeTo(null);
+        pantalla.setVisible(true);
+        
+    }
+}
